cdbac289581d5c8701d5419fb570d59645ae693a
Unknown <z_daz@email.com>
2018-02-27 21:13:57 +0200

    Committing Dragon RPG Project

---
+++ Assets/_Weapons/Ranged/Projectile.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Core; // TODO consider re-wiring
+
+namespace RPG.Weapons
+{
+    public class Projectile : MonoBehaviour
+    {
+
+        [SerializeField] float projectileSpeed;
+        [SerializeField] GameObject shooter; // so can inspect when paused
+
+        const float DESTROY_DELAY = 0.01f;
+        float damageCaused;
+
+        public void SetShooter(GameObject shooter)
+        {
+            this.shooter = shooter;
+        }
+
+        public void SetDamage(float damage)
+        {
+            damageCaused = damage;
+        }
+
+        public float GetProjectileLaunchSpeed()
+        {
+            return projectileSpeed;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            var layerCollidedWith = collision.gameObject.layer;
+            if (shooter && layerCollidedWith != shooter.layer)
+            {
+                DamageIfDamageable(collision);
+            }
+        }
+
+        private void DamageIfDamageable(Collision collision)
+        {
+            Component damageableComponent = collision.gameObject.GetComponent(typeof(IDamageable));
+            if (damageableComponent)
+            {
+                (damageableComponent as IDamageable).TakeDamage(damageCaused);
+            }
+            Destroy(gameObject, DESTROY_DELAY);
+        }
+    }
+}
